124da49046c8b38e38f74dfa362699efdab36793
Ivan Kashin <barbos1701@gmail.com>
2021-05-20 16:38:30 +0300

    Делает микросетки для главной для крошек и формы с фильтрами

--- src/components/page-content/page-content.jsx
+++ src/components/page-content/page-content.jsx
@@ -7,6 +7,8 @@ import ukulele2 from "../../assets/img/ukulele-2.png";
 import {ReactComponent as IconStar} from "../../assets/img/icon-star-whole.svg";
 import {ReactComponent as IconStarHalf} from "../../assets/img/icon-star-half.svg";
 import {ReactComponent as IconStarEmpty} from "../../assets/img/icon-star-empty.svg";
+import {ReactComponent as IconArrowRight} from "../../assets/img/icon-arrow-right.svg";
+import {ReactComponent as IconPriceLineSeparator} from "../../assets/img/icon-price-line-separator.svg";
 
 
 const PageContent = () => {
@@ -17,6 +19,9 @@ const PageContent = () => {
       <ul className="page-content__breadcrumbs breadcrumbs">
         <li className="breadcrumbs__item">
           <a href="#" className="breadcrumbs__link">Главная</a>
+        </li>
+        <li className="breadcrumbs__item breadcrumbs__item--arrow">
+          <IconArrowRight className="breadcrumbs__arrow" />
         </li>
         <li className="breadcrumbs__item">
           <a className="breadcrumbs__link breadcrumbs__link--current">Каталог</a>
@@ -31,39 +36,43 @@ const PageContent = () => {
             <fieldset className="guitar-filters__fieldset">
               <legend className="guitar-filters__legend">Цена, ₽</legend>
 
-              <label htmlFor="filters-price-from" className="visually-hidden">От</label>
-              <input className="guitar-filters__numeric-input" type="number" name="filters-price-from" id="filters-price-from" placeholder="1 000" />
+              <div className="guitar-filters__price-wrapper">
+                <label htmlFor="filters-price-from" className="visually-hidden">От</label>
+                <input className="guitar-filters__numeric-input" type="number" name="filters-price-from" id="filters-price-from" placeholder="1 000" />
 
-              <label htmlFor="filters-price-to" className="visually-hidden">До</label>
-              <input className="guitar-filters__numeric-input" type="number" name="filters-price-to" id="filters-price-to" placeholder="30 000" />
+                <IconPriceLineSeparator className="guitar-filters__price-separator" />
+
+                <label htmlFor="filters-price-to" className="visually-hidden">До</label>
+                <input className="guitar-filters__numeric-input" type="number" name="filters-price-to" id="filters-price-to" placeholder="30 000" />
+              </div>
             </fieldset>
 
             <fieldset className="guitar-filters__fieldset">
               <legend className="guitar-filters__legend">Тип гитар</legend>
 
-              <input className="guitar-filters__checkbox" type="checkbox" name="filters-type-acoustic" id="filters-type-acoustic" />
+              <input className="guitar-filters__checkbox visually-hidden" type="checkbox" name="filters-type-acoustic" id="filters-type-acoustic" />
               <label htmlFor="filters-type-acoustic" className="guitar-filters__checkbox-label">Акустические гитары</label>
 
-              <input className="guitar-filters__checkbox" type="checkbox" name="filters-type-electro" id="filters-type-electro" defaultChecked />
+              <input className="guitar-filters__checkbox visually-hidden" type="checkbox" name="filters-type-electro" id="filters-type-electro" defaultChecked />
               <label htmlFor="filters-type-electro" className="guitar-filters__checkbox-label">Электрогитары</label>
 
-              <input className="guitar-filters__checkbox" type="checkbox" name="filters-type-ukulele" id="filters-type-ukulele" defaultChecked />
+              <input className="guitar-filters__checkbox visually-hidden" type="checkbox" name="filters-type-ukulele" id="filters-type-ukulele" defaultChecked />
               <label htmlFor="filters-type-ukulele" className="guitar-filters__checkbox-label">Укулеле</label>
             </fieldset>
 
             <fieldset className="guitar-filters__fieldset">
               <legend className="guitar-filters__legend">Количество струн</legend>
 
-              <input className="guitar-filters__checkbox" type="checkbox" name="filters-strings-4" id="filters-strings-4" defaultChecked />
+              <input className="guitar-filters__checkbox visually-hidden" type="checkbox" name="filters-strings-4" id="filters-strings-4" defaultChecked />
               <label htmlFor="filters-strings-4" className="guitar-filters__checkbox-label">4</label>
 
-              <input className="guitar-filters__checkbox" type="checkbox" name="filters-strings-6" id="filters-strings-6" defaultChecked />
+              <input className="guitar-filters__checkbox visually-hidden" type="checkbox" name="filters-strings-6" id="filters-strings-6" defaultChecked />
               <label htmlFor="filters-strings-6" className="guitar-filters__checkbox-label">6</label>
 
-              <input className="guitar-filters__checkbox" type="checkbox" name="filters-strings-7" id="filters-strings-7" />
+              <input className="guitar-filters__checkbox visually-hidden" type="checkbox" name="filters-strings-7" id="filters-strings-7" />
               <label htmlFor="filters-strings-7" className="guitar-filters__checkbox-label">7</label>
 
-              <input className="guitar-filters__checkbox" type="checkbox" name="filters-strings-12" id="filters-strings-12" disabled />
+              <input className="guitar-filters__checkbox visually-hidden" type="checkbox" name="filters-strings-12" id="filters-strings-12" disabled />
               <label htmlFor="filters-strings-12" className="guitar-filters__checkbox-label">12</label>
             </fieldset>
 
