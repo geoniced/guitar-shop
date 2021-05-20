72b1693f9ccbab5c6d8e1ef038be974835803768
Ivan Kashin <barbos1701@gmail.com>
2021-05-20 09:04:08 +0300

    Подключает шрифты на страницу и прелоадит важный из них

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Helmet from "react-helmet";
 import App from "./components/app/app";
+import "./scss/style.scss";
+import droidSans from "./assets/fonts/droid-sans.woff2";
 
 ReactDOM.render(
   <React.StrictMode>
+    <Helmet>
+      <link rel="preload" href={droidSans} as="font" type="font/woff2" crossOrigin/>
+    </Helmet>
     <App />
   </React.StrictMode>,
   document.getElementById(`root`)
