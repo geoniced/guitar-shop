629d60c1ebf376d5a4efb85e994d2ab31a1551e1
Ivan Kashin <barbos1701@gmail.com>
2021-05-21 09:19:47 +0300

    Делает маршрутизацию в проекте

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,6 +1,7 @@
-import PageHeader from "../page-header/page-header"
-import PageContent from "../page-content/page-content"
-import PageFooter from "../page-footer/page-footer"
+import React from "react";
+import PageHeader from "../page-header/page-header";
+import PageContent from "../page-content/page-content";
+import PageFooter from "../page-footer/page-footer";
 
 const App = () => {
   return (
