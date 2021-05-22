38b3f0c159dc28ff78be9a097a9fc8985258d356
Ivan Kashin <barbos1701@gmail.com>
2021-05-22 18:45:24 +0300

    Отключает попапы

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -3,9 +3,9 @@ import {BrowserRouter as Router} from "react-router-dom";
 import PageHeader from "../page-header/page-header";
 import PageContent from "../page-content/page-content";
 import PageFooter from "../page-footer/page-footer";
-import AddToCartPopup from "../add-to-cart-popup/add-to-cart-popup";
-import ItemAddedToCartPopup from "../item-added-to-cart-popup/item-added-to-cart-popup";
-import DeleteFromCartPopup from "../delete-from-cart-popup/delete-from-cart-popup";
+// import AddToCartPopup from "../add-to-cart-popup/add-to-cart-popup";
+// import ItemAddedToCartPopup from "../item-added-to-cart-popup/item-added-to-cart-popup";
+// import DeleteFromCartPopup from "../delete-from-cart-popup/delete-from-cart-popup";
 
 
 const App = () => {
@@ -15,9 +15,9 @@ const App = () => {
       <PageContent />
 
       {/* TODO: Popup section for testing */}
-      <AddToCartPopup />
+      {/* <AddToCartPopup />
       <ItemAddedToCartPopup />
-      <DeleteFromCartPopup />
+      <DeleteFromCartPopup /> */}
 
       <PageFooter />
     </Router>
